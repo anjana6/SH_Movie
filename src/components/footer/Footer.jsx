a9b1f2a685459e58a84b33add82e423266a23580
anjana6 <anjanashakthi95@gmail.com>
2023-10-15 09:15:28 +0530

    created the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import './footer.scss'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.jpg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="footer_content container">
+        <div className="footer_content_logo">
+          <div className="logo">
+          <img src={logo} alt="logo" />
+          <Link to="/">Movies</Link>
+          </div>
+        </div>
+        <div className="footer_content_menus">
+          <div className="footer_content_menu">
+            <Link>Home</Link>
+            <Link>About Us</Link>
+            <Link>Contact</Link>
+          </div>
+          <div className="footer_content_menu">
+            <Link>Home</Link>
+            <Link>About Us</Link>
+          </div>
+          <div className="footer_content_menu">
+            <Link>Home</Link>
+            <Link>About Us</Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
