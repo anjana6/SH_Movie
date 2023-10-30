9dfd08996946bfd1cab3faec501f32bf9588f2f8
uzafW5wYXzeMaQpfxH5v <anjanashakthi95@gmail.com>
2023-10-30 09:59:36 +0530

    remove console log

--- server/src/controller/movie.controller.js
+++ server/src/controller/movie.controller.js
@@ -7,7 +7,6 @@ const fetchMovieDownloadLink = async (req, res) => {
         const links = await movieDownloadlink(body.name, body.year)
         res.status(200).json(links)
     } catch (error) {
-        console.log('eeeeeeeeeeee', error)
         res.status(400).json(error)
     }
 
